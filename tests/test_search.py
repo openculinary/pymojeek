03b16dab759a37d8f68806dbc23cf15f0716cab9
James Addison <james@reciperadar.com>
2023-09-19 17:53:55 +0100

    Add basic test coverage for the 'qm' word exclusion list parameter

--- tests/test_search.py
+++ tests/test_search.py
@@ -23,3 +23,18 @@ class SearchTest(TestCase):
         outbound_request = mock_urlopen.call_args_list[0].args[0]
         requested_url = outbound_request.get_full_url()
         self.assertIn("&q=testing+testing+one+two+three", requested_url)
+
+    @patch("pymojeek.urlopen")
+    def test_word_exclusion_query(self, mock_urlopen: Mock) -> None:
+        mock_urlopen.return_value = SearchTest.MockResponse()
+
+        client = Search(api_key="000000000000000000000000")
+        client.search(
+            "testing testing one two three",
+            exclude_words=["ignore", "these"],
+        )
+
+        self.assertTrue(mock_urlopen.called)
+        outbound_request = mock_urlopen.call_args_list[0].args[0]
+        requested_url = outbound_request.get_full_url()
+        self.assertIn("&qm=ignore+these", requested_url)
